ea1ea8ecf12a3731627ada7b317ee87bdd442f08
Vinicius Barbosa Santos <viniciusfoxsantos@gmail.com>
2023-11-21 19:47:24 -0300

    Show notes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -5,6 +5,7 @@ const notesRoutes = Router()
 const NotesController = require('../controllers/NotesController');
 const notesController = new NotesController()
 
-notesRoutes.post('/', notesController.create)
+notesRoutes.post('/:user_id', notesController.create)
+notesRoutes.get('/:id', notesController.show)
 
 module.exports = notesRoutes
