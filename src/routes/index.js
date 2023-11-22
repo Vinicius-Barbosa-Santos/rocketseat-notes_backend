df51cad5f3ce78de6e2bee6d29b1bc79a232f01d
Vinicius Barbosa Santos <viniciusfoxsantos@gmail.com>
2023-11-21 23:01:20 -0300

    Notes with tags and show tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ const routes = Router()
 
 const userRoutes = require('../routes/user.routes')
 const notesRoutes = require('../routes/notes.routes')
+const tagsRoutes = require('../routes/tags.routes')
 
 routes.use('/users', userRoutes)
 routes.use('/notes', notesRoutes)
+routes.use('/tags', tagsRoutes)
 
 module.exports = routes
